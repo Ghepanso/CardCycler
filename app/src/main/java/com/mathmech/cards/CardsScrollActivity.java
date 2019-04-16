916f4a6dd8dd2ebb47e76dfdc4f11ac8b1dfbb59
Jubick <mattyson.so@gmail.com>
2019-04-16 15:06:18 +0500

    Added Logic

--- app/src/main/java/com/mathmech/cards/CardsScrollActivity.java
+++ app/src/main/java/com/mathmech/cards/CardsScrollActivity.java
@@ -3,8 +3,6 @@ package com.mathmech.cards;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -12,44 +10,56 @@ import java.security.KeyException;
 import java.util.ArrayList;
 
 public class CardsScrollActivity extends AppCompatActivity {
-
+    TextView tipsView;
     TextView themeView;
     TextView questionView;
     Button nextButton;
+    Button showTipsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cards_scroll);
-        themeView = findViewById(R.id.question);
+
+        themeView = findViewById(R.id.cards_theme);
+        showTipsButton = findViewById(R.id.showTips);
+        tipsView = findViewById(R.id.tips);
+        nextButton = findViewById(R.id.buttonNext);
+        questionView = findViewById(R.id.question);
+
         Intent intent = getIntent();
         String theme = intent.getStringExtra("Cards name");
         themeView.setText(theme);
 
         CardCycler cardCycler = new CardCycler(getAssets());
         Cycler cycler = null;
-        try
-        {
-            Packet packet = cardCycler.getPacket("Osmin");
+        try {
+            Packet packet = cardCycler.getPacket(theme);
             ArrayList<Card> cards = packet.getCards(cardCycler.unpacker);
             cycler = new Cycler(cards);
-        }
-        catch (KeyException e)
-        {
+        } catch (KeyException e) {
             e.printStackTrace();
         }
 
-        nextButton = findViewById(R.id.buttonNext);
         Cycler finalCycler = cycler;
-        nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i("CLK", finalCycler.currentCard.name);
-                if (finalCycler != null) {
-                    questionView.setText(finalCycler.currentCard.question);
-                    finalCycler.setNextCard();
-                }
+        questionView.setText(finalCycler.currentCard.question);
+
+        StringBuilder sb = new StringBuilder();
+        final int[] i = {0};
+        nextButton.setOnClickListener(v -> {
+            if (finalCycler != null) {
+                questionView.setText(finalCycler.currentCard.question);
+                tipsView.setText("");
+                sb.delete(0, sb.length());
+                i[0] = 0;
+                finalCycler.setNextCard();
             }
+        });
+
+        showTipsButton.setOnClickListener(v -> {
+            sb.append(finalCycler.currentCard.tips[i[0]] + '\n');
+            tipsView.setText(sb.toString());
+            i[0]++;
         });
     }
 
