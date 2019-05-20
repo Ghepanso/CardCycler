b1667578a6a9c17a874a7e2fccd1e26871b2dc7f
Jubick <mattyson.so@gmail.com>
2019-05-20 23:53:14 +0500

    fixed cards count which was an array cause of inner class

--- app/src/main/java/com/mathmech/cards/CardsScrollActivity.java
+++ app/src/main/java/com/mathmech/cards/CardsScrollActivity.java
@@ -1,7 +1,6 @@
 package com.mathmech.cards;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -22,6 +21,7 @@ public class CardsScrollActivity extends AppCompatActivity {
     TextView questionView;
     LinearLayout swipeableView;
     Animation fade;
+    int cardsCount = 0;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -35,8 +35,7 @@ public class CardsScrollActivity extends AppCompatActivity {
         questionView = findViewById(R.id.question);
         fade = AnimationUtils.loadAnimation(this, R.anim.fade);
 
-        Intent intent = getIntent();
-        String theme = intent.getStringExtra("Cards name");
+        String theme = getIntent().getStringExtra("Cards name");
         themeView.setText(theme);
 
         CardCycler cardCycler = new CardCycler(getAssets());
@@ -52,10 +51,9 @@ public class CardsScrollActivity extends AppCompatActivity {
         Cycler finalCycler = cycler;
         questionView.setText(Objects.requireNonNull(finalCycler).currentCard.getQuestion());
 
-        StringBuilder sb = new StringBuilder();
-        final int[] i = {0};
-
         swipeableView.setOnTouchListener(new OnSwipeTouchListener(CardsScrollActivity.this) {
+            StringBuilder sb = new StringBuilder();
+
             @Override
             public void onSwipeLeft() {
                 fade.reset();
@@ -65,7 +63,7 @@ public class CardsScrollActivity extends AppCompatActivity {
                 tipsView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
                 tipsView.setText("");
                 sb.delete(0, sb.length());
-                i[0] = 0;
+                cardsCount = 0;
                 finalCycler.setNextCard();
                 questionView.setText(finalCycler.currentCard.getQuestion());
             }
@@ -73,11 +71,11 @@ public class CardsScrollActivity extends AppCompatActivity {
             @Override
             public void onSwipeRight() {
                 TextViewCompat.setAutoSizeTextTypeWithDefaults(tipsView, TextViewCompat.AUTO_SIZE_TEXT_TYPE_UNIFORM);
-                if (finalCycler.currentCard.getTips().length > i[0]) {
+                if (finalCycler.currentCard.getTips().length > cardsCount) {
                     sb.append("\t\t");
-                    sb.append(finalCycler.currentCard.getTips()[i[0]]).append('\n');
+                    sb.append(finalCycler.currentCard.getTips()[cardsCount]).append('\n');
                     tipsView.setText(sb.toString());
-                    i[0]++;
+                    cardsCount++;
                 } else {
                     Toast toast = Toast.makeText(getApplicationContext(),
                             "Подсказок больше нет", Toast.LENGTH_SHORT);
