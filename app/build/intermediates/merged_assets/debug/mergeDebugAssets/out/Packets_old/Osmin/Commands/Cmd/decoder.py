6c60b6cb24594e2f4dc3ed52599fb158351554b0
Jubick <mattyson.so@gmail.com>
2019-05-21 19:48:11 +0500

    Fixed cycler holder etc

---
+++ app/build/intermediates/merged_assets/debug/mergeDebugAssets/out/Packets_old/Osmin/Commands/Cmd/decoder.py
@@ -0,0 +1,30 @@
+import os
+from chardet import detect
+import glob, os
+
+for file in glob.glob("*.txt"):
+    print(file)
+    srcfile = file
+    trgfile = file + "asd"
+
+
+    def get_encoding_type(file):
+        with open(file, 'rb') as f:
+            rawdata = f.read()
+        return detect(rawdata)['encoding']
+
+
+    from_codec = 'cp1251'
+
+    # add try: except block for reliability
+    try:
+        with open(srcfile, 'r', encoding=from_codec) as f, open(trgfile, 'w', encoding='utf-8') as e:
+            text = f.read()  # for small files, for big use chunks
+            e.write(text)
+
+        os.remove(srcfile)  # remove old encoding file
+        os.rename(trgfile, srcfile)  # rename new encoding
+    except UnicodeDecodeError:
+        print('Decode Error')
+    except UnicodeEncodeError:
+        print('Encode Error')
